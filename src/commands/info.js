d826b79b17f0fcc8afadfa8cbae664277b789cf7
Conni <connithekiwi@gmail.com>
2021-03-28 04:22:33 +0100

    New command.

---
+++ src/commands/info.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+exports.run = (bot, msg, args) => {
+    msg.channel.send(`Version: 1.0.0.0`);
+}
+
+exports.test = (bot) => {
+
+}
+
+exports.help = {
+    name: 'info',
+    usage: 'info',
+    description: 'View Bot Infomation.'
+}

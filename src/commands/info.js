51c11f660799a36677161852abb0d28a719db5b6
Conni <connithekiwi@gmail.com>
2021-03-28 04:40:10 +0100

    Add version to file

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,6 +1,19 @@
 // eslint-disable-next-line no-unused-vars
+
+var version = "";
+fs.readFile('../../Version.V', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    Version = data;
+});
+
+function getVersion() {
+    return version;
+}
+
 exports.run = (bot, msg, args) => {
-    msg.channel.send(`Version: 1.0.0.0`);
+    msg.channel.send(`Version: ${getVersion()}`);
 }
 
 exports.test = (bot) => {

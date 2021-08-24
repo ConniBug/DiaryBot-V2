14e4a0d26a4a6abef54c3c2b25d32cc3d5ea36ea
Conni Bug <connorbilham9@googlemail.com>
2021-08-24 04:16:03 +0100

    Updated the version command to use package.json data.

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,45 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 const fs = require('fs')
 
-var version = "";
-fs.readFile('./Version.V', 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(`Version ${version}`)
-    Version = data;
-});
+var version = require("../../package.json").version;
 
-function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-}  
-
-async function getVersion() {
-    if(version == "") {
-        fs.readFile('./Version.V', 'utf8', function (err,data) {
-            if (err) {
-              return console.log(err);
-            }
-            if(version == "") {
-                if(data != version) {
-                    console.log(`version is: ${data}`);
-                    version = data
-                } 
-            }
-            return version;
-        });
-    } else {
-        return version;
-    }
-    await sleep(1000);
-    return version;
-}
 
 exports.run = async(bot, msg, args) => {
-    await getVersion();
-    msg.channel.send(`Version: ${await getVersion()}`);
+    msg.channel.send(`Version: ${version}`);
 }
 
 exports.test = (bot) => {
